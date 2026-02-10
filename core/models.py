d30273ced336b7625b3991384c64716c7145c875
anthphan <89223273+anthphan@users.noreply.github.com>
2026-02-10 18:36:10 -0500

    feat: add PlayerMatchStats model

--- core/models.py
+++ core/models.py
@@ -26,3 +26,20 @@ class Match(models.Model):
 
     def __str__(self):
         return f"Patch {self.patch} @ {self.played_at}"
+
+
+class PlayerMatchStats(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+
+    champion = models.CharField(max_length=50)
+
+    kills = models.IntegerField()
+    deaths = models.IntegerField()
+    assits = models.IntegerField()
+
+    cs = models.IntegerField()
+    gold = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.player} in match {self.match}"
