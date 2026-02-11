cd22dcfb0e53b40aaf0794e69c6886e525122057
anthphan <89223273+anthphan@users.noreply.github.com>
2026-02-10 21:27:53 -0500

    feat: add game start offset to VOD

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Player, PlayerMatchStats, Match, ObjectiveEvent
+from .models import Player, PlayerMatchStats, Match, ObjectiveEvent, Vod
 from django.db.models import Avg
 
 
@@ -35,9 +35,11 @@ def match_detail(request, match_id):
 
     stats = PlayerMatchStats.objects.filter(match=match)
     objectives = ObjectiveEvent.objects.filter(match=match).order_by("minute")
+    vod = Vod.objects.filter(match=match).first()
 
     return render(request, "match_detail.html", {
         "match": match,
         "stats": stats,
         "objectives": objectives,
+        "vod": vod,
     })
